0d9125861ca3b5812cca535e30c8872a38e58645
Charan <sai.peram@everest.engineering>
2021-11-24 13:11:22 +0530

    added methods to floor class and create parking lot using singleton

--- src/main/java/com/charan/parkingsystem/parking/ParkingFloor.java
+++ src/main/java/com/charan/parkingsystem/parking/ParkingFloor.java
@@ -1,4 +1,34 @@
 package com.charan.parkingsystem.parking;
 
+import com.charan.parkingsystem.Types.VehicleType;
+
+import java.util.List;
+
 public class ParkingFloor {
+    private int floorNo;
+    private List<ParkingSlot> floorSlots;
+    private boolean isFull;
+    private List<ParkingSlot> availableSlotsByType;
+
+    public ParkingFloor(int floorNo, List<ParkingSlot> floorSlots ){
+        this.floorNo = floorNo;
+        this.floorSlots = floorSlots;
+    }
+
+    public boolean checkIsFull(){
+        return isFull;
+    }
+
+    public List<ParkingSlot> findSlotsByType(VehicleType vehicleType){
+        for(ParkingSlot slot: floorSlots){
+            if(slot.getIsFree() && slot.getSpotType() == vehicleType){
+                availableSlotsByType.add(slot);
+            }
+        }
+        return availableSlotsByType;
+    }
+
+    public List<ParkingSlot> getFloorSlots() {
+        return floorSlots;
+    }
 }
--- src/main/java/com/charan/parkingsystem/parking/ParkingLot.java
+++ src/main/java/com/charan/parkingsystem/parking/ParkingLot.java
@@ -1,4 +1,29 @@
 package com.charan.parkingsystem.parking;
 
 public class ParkingLot {
+    private String id = "PR1234";
+    private int slotsPerFloor;
+    private int noOfFloors;
+
+    private static ParkingLot instance;
+
+    private ParkingLot(String id, int floors, int slotsPerFloor){
+        this.id = id;
+        this.noOfFloors = floors;
+        this.slotsPerFloor = slotsPerFloor;
+    }
+
+    public static ParkingLot getInstance(){
+        if(instance == null){
+            throw new AssertionError("Call the init first");
+        }
+        return instance;
+    }
+    public static synchronized ParkingLot init(String id, int floors, int slots){
+        if(instance != null){
+            throw new AssertionError("Initialisation done already");
+        }
+        instance = new ParkingLot(id, floors, slots);
+        return instance;
+    }
 }
